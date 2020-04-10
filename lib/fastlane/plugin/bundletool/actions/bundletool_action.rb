4a995d350a54029a48e9912336b8981aca6e9662
Martin Gonzalez <martin.gonzalez@etermax.com>
2020-04-10 14:38:23 -0300

    Added validations tests

--- lib/fastlane/plugin/bundletool/actions/bundletool_action.rb
+++ lib/fastlane/plugin/bundletool/actions/bundletool_action.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'fastlane/action'
 require_relative '../helper/bundletool_helper'
 
@@ -10,7 +12,7 @@ module Fastlane
         @bundletool_temp_path = "#{@project_root}/bundletool_temp"
         @verbose = params[:verbose]
         keystore_info = {}
-        unless (params[:ks_path].nil?)
+        unless params[:ks_path].nil?
           keystore_info[:keystore_path] = params[:ks_path]
           keystore_info[:keystore_password] = params[:ks_password]
           keystore_info[:alias] = params[:ks_key_alias]
@@ -19,45 +21,48 @@ module Fastlane
 
         bundletool_version = params[:bundletool_version]
         aab_path = params[:aab_path]
-        output_path = params[:apk_output_path]
+        output_path = params[:apk_output_path] || '.'
 
-        validate_aab!(aab_path) 
-        download_bundletool(bundletool_version)
+        return unless validate_aab!(aab_path)
+
+        return unless download_bundletool(bundletool_version)
+
         extract_universal_apk_from(aab_path, output_path, keystore_info)
       end
 
       def self.validate_aab!(aab_path)
         puts_message('Checking if .aab file exists...')
-        puts_error!(".aab file at #{aab_path} does not exist") unless File.file?(aab_path)
+        unless File.file?(aab_path)
+          puts_error!(".aab file at #{aab_path} does not exist")
+          return false
+        end
         puts_success('Checking if .aab file exists')
       end
 
       def self.download_bundletool(version)
-        begin
-          puts_message("Downloading bundletool (#{version}) from https://github.com/google/bundletool/releases/download/#{version}/bundletool-all-#{version}.jar...")
-          Dir.mkdir "#{@project_root}/bundletool_temp"
-          open("https://github.com/google/bundletool/releases/download/#{version}/bundletool-all-#{version}.jar") do |bundletool|
-            File.open("#{@bundletool_temp_path}/bundletool.jar", "wb") do |file|
-              file.write(bundletool.read)
-            end
+        puts_message("Downloading bundletool (#{version}) from https://github.com/google/bundletool/releases/download/#{version}/bundletool-all-#{version}.jar...")
+        Dir.mkdir "#{@project_root}/bundletool_temp"        
+        Kernel.open("https://github.com/google/bundletool/releases/download/#{version}/bundletool-all-#{version}.jar") do |bundletool|
+          File.open("#{@bundletool_temp_path}/bundletool.jar", 'wb') do |file|
+            file.write(bundletool.read)
           end
-          puts_success('Downloading bundletool')
-        rescue StandardError => e
-          puts_error!("Something went wrong when downloading bundletool version #{version}. \nError message\n #{e.message}")      
         end
+        puts_success('Downloading bundletool')
+      rescue StandardError => e
+        clean_temp!
+        puts_error!("Something went wrong when downloading bundletool version #{version}. \nError message\n #{e.message}")        
+        false
       end
 
       def self.extract_universal_apk_from(aab_path, apk_output_path, keystore_info)
-        begin
-          aab_absolute_path = Pathname.new(File.expand_path(aab_path)).to_s
-          apk_output_absolute_path = Pathname.new(File.expand_path(apk_output_path)).to_s
-          output_path = run_bundletool!(aab_absolute_path, keystore_info)
-          prepare_apk!(output_path, apk_output_absolute_path)
-        rescue StandardError => e          
-          puts_error!("Bundletool could not extract universal apk from aab at #{aab_absolute_path}. \nError message\n #{e.message}")          
-        ensure
-          clean_temp!
-        end
+        aab_absolute_path = Pathname.new(File.expand_path(aab_path)).to_s
+        apk_output_absolute_path = Pathname.new(File.expand_path(apk_output_path)).to_s
+        output_path = run_bundletool!(aab_absolute_path, keystore_info)
+        prepare_apk!(output_path, apk_output_absolute_path)
+      rescue StandardError => e
+        puts_error!("Bundletool could not extract universal apk from aab at #{aab_absolute_path}. \nError message\n #{e.message}")
+      ensure
+        clean_temp!
       end
 
       def self.run_bundletool!(aab_path, keystore_info)
@@ -65,18 +70,11 @@ module Fastlane
         output_path = "#{@bundletool_temp_path}/output.apks"
         keystore_params = ''
 
-        unless (keystore_info.empty?)
-          keystore_params = "--ks=#{keystore_info[:keystore_path]} \
-          --ks-pass=pass:#{keystore_info[:keystore_password]} \
-          --ks-key-alias=#{keystore_info[:alias]} \
-          --key-pass=pass:#{keystore_info[:alias_password]}"
+        unless keystore_info.empty?
+          keystore_params = "--ks=#{keystore_info[:keystore_path]} --ks-pass=pass:#{keystore_info[:keystore_password]} --ks-key-alias=#{keystore_info[:alias]} --key-pass=pass:#{keystore_info[:alias_password]}"
         end
 
-        cmd = "java -jar #{@bundletool_temp_path}/bundletool.jar build-apks \
-        --bundle=#{aab_path} \
-        --output=#{output_path} \
-        --mode=universal \
-        #{keystore_params}"
+        cmd = "java -jar #{@bundletool_temp_path}/bundletool.jar build-apks --bundle=#{aab_path} --output=#{output_path} --mode=universal #{keystore_params}"
 
         Open3.popen3(cmd) do |_, _, stderr, wait_thr|
           exit_status = wait_thr.value
@@ -88,7 +86,9 @@ module Fastlane
 
       def self.prepare_apk!(output_path, target_path)
         puts_message("Preparing apk to #{target_path}...")
-        puts_important("Apk at path #{target_path} exists. Replacing it.") if File.file?(target_path)
+        if File.file?(target_path)
+          puts_important("Apk at path #{target_path} exists. Replacing it.")
+        end
         target_dir_name = File.dirname(target_path)
         unless Dir.exist?(target_dir_name)
           puts_important("Creating path #{target_dir_name} since does not exist")
@@ -104,7 +104,7 @@ module Fastlane
         puts_success("Preparing apk to #{target_path}")
       end
 
-      def self.clean_temp!()
+      def self.clean_temp!
         cmd = "rm -rf #{@project_root}/bundletool_temp"
         Open3.popen3(cmd) do |_, _, stderr, wait_thr|
           exit_status = wait_thr.value
@@ -117,14 +117,19 @@ module Fastlane
       end
 
       def self.puts_success(message)
-        UI.success "#{message} #{Fastlane::Helper::Emojis.green_checkmark}" if @verbose
+        if @verbose
+          UI.success "#{message} #{Fastlane::Helper::Emojis.green_checkmark}"
+        end
       end
 
       def self.puts_important(message)
-        UI.important "#{message} #{Fastlane::Helper::Emojis.warning}" if @verbose
+        if @verbose
+          UI.important "#{message} #{Fastlane::Helper::Emojis.warning}"
+        end
       end
 
       def self.puts_error!(message)
+        p message
         UI.user_error! message
       end
 
@@ -162,12 +167,8 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :apk_output_path,
                                        description: 'Path where the apk file is going to be placed',
                                        is_string: true,
-                                       optional: false,
-                                       verify_block: proc do |value|
-                                                       unless value && !value.empty?
-                                                         UI.user_error!('You must set apk_output_path.')
-                                                       end
-                                                     end),
+                                       optional: true,
+                                       default_value: '.'),
           FastlaneCore::ConfigItem.new(key: :verbose,
                                        description: 'Show every messages of the action',
                                        is_string: false,
@@ -181,20 +182,20 @@ module Fastlane
       def self.print_params(options)
         table_title = "Params for bundletool #{Fastlane::Bundletool::VERSION}"
         FastlaneCore::PrintTable.print_values(config: options,
-                                              mask_keys: [:ks_password, :ks_key_alias_password],
+                                              mask_keys: %i[ks_password ks_key_alias_password],
                                               title: table_title)
       end
 
       def self.description
-        "Extracts an universal apk from an .aab file"
+        'Extracts an universal apk from an .aab file'
       end
 
       def self.authors
-        ["Martin Gonzalez"]
+        ['Martin Gonzalez']
       end
 
       def self.details
-        "Using the google oficial bundletool to extract an universal apk from .aab file to distribute it"
+        'Using the google oficial bundletool to extract an universal apk from .aab file to distribute it'
       end
 
       def self.is_supported?(platform)
--- spec/bundletool_action_spec.rb
+++ spec/bundletool_action_spec.rb
@@ -1,14 +1,38 @@
-describe Fastlane::Actions::BundletoolAction do
-  describe '#run' do
-    it 'throws UI.user_error! when providing invalid .aab file path' do
-      # invalid_path = "some_invalid_dir/exmaple.aab"
-      # expect(Fastlane::Actions::BundletoolAction).to receive(:puts_error!).with(".aab file at #{invalid_path} does not exist")
+# frozen_string_literal: true
 
-      # Fastlane::Actions::BundletoolAction.run(verbose: true,
-      #   bundletool_version: '0.11.0',
-      #   aab_path: invalid_path,
-      #   apk_output_path: '/resources/example.apk'
-      # )
-    end
+describe 'BundletoolAction.run should' do
+  it 'throws error when bundletool fails' do
+    aab_path = 'path/example.aab'
+    aab_absolute_path = Pathname.new(File.expand_path(aab_path)).to_s
+    bundletool_url = 'https://github.com/google/bundletool/releases/download/0.11.0/bundletool-all-0.11.0.jar'
+    error = 'error'
+    allow(File).to receive(:file?).with(aab_path).and_return(true)
+    allow(Kernel).to receive(:open).with(bundletool_url).and_return('somebody')
+    allow(Fastlane::Actions::BundletoolAction).to receive(:run_bundletool!).and_raise(StandardError.new(error))
+    expect(FastlaneCore::UI).to receive(:user_error!).with("Bundletool could not extract universal apk from aab at #{aab_absolute_path}. \nError message\n #{error}")
+    Fastlane::Actions::BundletoolAction.run(verbose: true,
+                                            bundletool_version: '0.11.0',
+                                            aab_path: aab_path)
+  end
+  it 'throws error when downloading bundletool fails' do
+    aab_path = 'path/example.aab'
+    allow(File).to receive(:file?).with(aab_path).and_return(true)
+    bundletool_url = 'https://github.com/google/bundletool/releases/download/0.11.0/bundletool-all-0.11.0.jar'
+    allow(Kernel).to receive(:open).with(bundletool_url).and_raise(StandardError.new('error'))
+
+    expect(FastlaneCore::UI).to receive(:user_error!).with("Something went wrong when downloading bundletool version 0.11.0. \nError message\n error")
+
+    Fastlane::Actions::BundletoolAction.run(verbose: true,
+                                            bundletool_version: '0.11.0',
+                                            aab_path: aab_path,
+                                            apk_output_path: '/resources/example.apk')
+  end
+  it 'throws .aab file does not exist UI.user_error! when providing invalid .aab file path' do
+    invalid_path = 'some_invalid_dir/example.aab'
+    expect(FastlaneCore::UI).to receive(:user_error!).with('.aab file at some_invalid_dir/example.aab does not exist')
+    Fastlane::Actions::BundletoolAction.run(verbose: true,
+                                            bundletool_version: '0.11.0',
+                                            aab_path: invalid_path,
+                                            apk_output_path: '/resources/example.apk')
   end
 end
